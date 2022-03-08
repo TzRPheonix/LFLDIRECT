4efe24d5af6f62c121634ea1be077165a746f1e8
tzrph <TzRPheonix@gmail.com>
2022-03-08 16:34:12 +0100

    Nettoyage

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setTitle("Avec la branche c mieux");
+        setTitle("LFL DIRECT");
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
